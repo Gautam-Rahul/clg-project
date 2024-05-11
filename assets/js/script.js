bb4c9fbf0258b17e4c3acd133cc9ba96d6dbc55d
Rahul Gautam <61191660+Gautam-Rahul@users.noreply.github.com>
2024-05-11 13:26:38 +1000

    changes made 4e

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,3 +168,32 @@ window.addEventListener("mousemove", function (event) {
   }
 
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("readMoreBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  overlay.style.display = "block"; // Show the overlay
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  overlay.style.display = "none"; // Hide the overlay
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    overlay.style.display = "none"; // Hide the overlay
+  }
+}
